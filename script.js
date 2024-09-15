c0ff0f9d1723a14a7c62578a2d5e5e9428089fb1
Ahmed <Ahmedoffical.110@gmail.com>
2024-09-15 15:22:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,104 @@
+
+// var age = prompt("enter your age");
+// var currentYear =  new Date().getFullYear()
+// var sum =  currentYear - age 
+// alert(sum)
+
+
+
+// Home assignment 
+
+// var yearOfBirth = prompt("Please enter your year of birth:");
+// yearOfBirth = Number(yearOfBirth);
+// var currentYear = new Date().getFullYear();
+// var age = currentYear - yearOfBirth;
+// alert("You are " + age + " years old.");
+
+
+
+// var myName=prompt("enter the day")
+
+// if(myName=="Sunday"){
+//     alert("Happy sunday")
+
+// } else {
+//     alert('sorry')
+// }
+
+
+
+// var yourName=prompt("Enter the name")
+
+// if(yourName=="Ahmed"){
+//     alert("Hi Ahmed")
+// }else{
+//     alert("Your not Ahmed")
+
+
+// }
+
+// Question no : 1 
+
+// var myNumber=prompt("Enter a number")
+
+// if(myNumber % 2 ===0){
+//     alert("number is even")
+// }
+// else{
+//     alert("Number is odd")
+// }
+
+
+// Question no : 2
+
+// var userAge=prompt("enter your age")
+
+// if(userAge==18){
+//     alert("You are eligible for voting")
+// }
+// else{
+//     alert("Sorry your are not eligible")
+// }
+
+
+// Question no : 3
+
+// var userNum=prompt("enter your marks")
+// var grade;
+
+// if( userNum >=90){
+//     grade="A"
+// }
+// else if( userNum >=80){
+//     grade="B"
+// }else if( userNum >=70){
+//     grade="c"
+// }else if( userNum >=60){
+//     grade="D"
+// }else{
+//     grade="fail"
+// }
+
+// alert("Enter your grade" + " " + grade)
+
+
+
+// Question no : 4 
+
+// var userNum=prompt("enter number")
+
+// if(userNum==1234){
+//     alert("Access granted")
+// }
+// else(alert("Access denied"))
+
+
+// Question no : 5 
+
+// var userWords =prompt("enter your words");
+// if(userWords=="a"||userWords=="e"||userWords=="i"||userWords=="o"|| userWords=="u"){
+//     alert(userWords + "" + "this is vowel")
+// }
+// else{
+//     alert(userWords + "" + "this is not vowle ")
+// }
